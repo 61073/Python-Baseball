82cc69238c9a35a7656950f2e1728bf8794db470
61073 <mike.wood@mastek.com>
2019-03-27 10:04:30 +0000

    fixxxxxx

--- stats/offense.py
+++ stats/offense.py
@@ -9,7 +9,7 @@ plays.columns = ['type', 'inning', 'team', 'player', 'count', 'pitches', 'event'
 hits = plays.loc[plays['event'].str.contains('^(?:S(?!B)|D|T|HR)'), ['inning', 'event']]
 hits.loc[:, 'inning'] = pd.to_numeric(hits.loc[:, 'inning'])
 
-replacements = { r'^S(.*)': 'single', r'^D(.*)': 'double', r'^T(.*)': 'triple', r'^HE(.*)': 'hr' }
+replacements = { r'^S(.*)': 'single', r'^D(.*)': 'double', r'^T(.*)': 'triple', r'^HR(.*)': 'hr' }
 hit_type = hits['event'].replace(replacements, regex=True)
 hits = hits.assign(hit_type=hit_type)
 
@@ -17,9 +17,9 @@ hits = hits.groupby(['inning', 'hit_type']).size().reset_index(name='count')
 
 hits['hit_type'] = pd.Categorical(hits['hit_type'], ['single', 'double', 'triple', 'hr'])
 
-# hits = hits.sort_values(['inning', 'hit_type'])
+hits = hits.sort_values(['inning', 'hit_type'])
 
-# hits = hits.pivot(index='inning', columns='hit_type', values='count')
+hits = hits.pivot(index='inning', columns='hit_type', values='count')
 
-# hits.plot.bar(stacked=True)
-# plt.show()
+hits.plot.bar(stacked=True)
+plt.show()
